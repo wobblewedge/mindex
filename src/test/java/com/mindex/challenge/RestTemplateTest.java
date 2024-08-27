931e9a8c5a662fa670de96b31ed80277fe13a6e2
mindex <dev@mindex.com>
2024-08-27 18:23:37 -0400

    Reformat one last time

--- src/test/java/com/mindex/challenge/RestTemplateTest.java
+++ src/test/java/com/mindex/challenge/RestTemplateTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
@@ -27,9 +30,6 @@ public class RestTemplateTest {
     private String reportingStructureUrl;
     private String compensationUrl;
     private static Employee testEmployee;
-
-    @Autowired
-    private EmployeeService employeeService;
 
     @LocalServerPort
     private int port;
